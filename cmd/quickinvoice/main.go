1110603f598c9fbdc49cd92f351073162cdcffd8
Milos Zivlak <zivlakmilos@gmail.com>
2024-02-10 15:37:23 +0100

    Generate invoice endpoint

--- cmd/quickinvoice/main.go
+++ cmd/quickinvoice/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/zivlakmilos/quickinvoice/pkg/quickinvoice"
 )
 
 func main() {
@@ -15,6 +16,15 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
+	})
+
+	e.POST("/invoice", func(c echo.Context) error {
+		data, err := quickinvoice.DecodeJson(c.Request().Body)
+		if err != nil {
+			return err
+		}
+		err = quickinvoice.GenerateInvoice(data, c.Response())
+		return err
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
